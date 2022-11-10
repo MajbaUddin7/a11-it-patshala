d53977a93371988ea0850cc93c5508a852dda8fe
majbauddi7 <majbauddinsabbir7@gmail.com>
2022-11-10 23:16:17 +0600

    servicebox add

--- src/Pages/Join/LogIn/LogIn.js
+++ src/Pages/Join/LogIn/LogIn.js
@@ -1,4 +1,4 @@
-// import { Card } from '@material-tailwind/react';
+import { Card } from '@material-tailwind/react';
 import { Button } from '@material-tailwind/react';
 import React, { useContext, useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
