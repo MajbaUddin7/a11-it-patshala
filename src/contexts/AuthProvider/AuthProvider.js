c7a88e91ea378b8dc90a19cde6bd01abc6ac7a8f
majbauddi7 <majbauddinsabbir7@gmail.com>
2022-11-10 23:04:19 +0600

    AuthProvider setup

--- src/contexts/AuthProvider/AuthProvider.js
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -33,7 +33,7 @@ const AuthProvider = ({ children }) => {
     }
 
     const logOut = () => {
-        // localStorage.removeItem('genius-token');
+        localStorage.removeItem('genius-token');
         return signOut(auth);
     }
 
